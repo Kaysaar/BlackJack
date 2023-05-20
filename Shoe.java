92fb0012810031c5eee49b74b999a139d3e9a14b
dziwicz123 <michal.dziwicz123@gmail.com>
2023-05-20 17:53:23 +0200

    Corectiong amount of decks in Shoe

--- Shoe.java
+++ Shoe.java
@@ -4,7 +4,7 @@ public class Shoe {
     private ArrayList<Deck> shoe = new ArrayList<>();
 
     public Shoe(){
-        for(int i = 0; i < 9; i++){
+        for(int i = 0; i < 8; i++){
             Deck deck = new Deck();
             shoe.add(deck);
         }
