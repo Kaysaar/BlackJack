8d12579f2d633575e60d8e3fbb3ee48eee10c489
LuckyCoin2 <240730@edu.p.lodz.pl>
2023-05-23 18:12:50 +0200

    Modifing application to run homepage

--- lwjgl3/src/main/java/com/duszki/blackjack/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/duszki/blackjack/lwjgl3/Lwjgl3Launcher.java
@@ -3,6 +3,7 @@ package com.duszki.blackjack.lwjgl3;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.duszki.blackjack.Blackjack;
+import com.duszki.blackjack.Homepage;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
@@ -11,7 +12,7 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Blackjack(), getDefaultConfiguration());
+        return new Lwjgl3Application(new Homepage(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
