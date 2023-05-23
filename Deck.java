f463d89fb03b302487b902d5f689b9549f8e9297
dziwicz123 <michal.dziwicz123@gmail.com>
2023-05-23 14:34:36 +0200

    Creation of new ArrayList to constructor

--- Deck.java
+++ Deck.java
@@ -2,9 +2,10 @@ import java.util.ArrayList;
 
 public class Deck {
     public static final int STRONGCARD = 10;
-    private ArrayList<Card> deck = new ArrayList<>();
+    private ArrayList<Card> deck;
 
     public Deck(){
+        deck = new ArrayList<>();
         for (int j = 1; j<5; j++) {
             for (int i = 2; i < 12; i++) {
                 Card card = new Card(i);
