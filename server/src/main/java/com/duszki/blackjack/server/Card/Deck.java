8ab50d27a55a2b2e90d7b7a87acb8e32752c2f07
dziwicz123 <michal.dziwisz@poczta.onet.pl>
2023-05-23 18:10:59 +0200

    Criation of Deck class

---
+++ server/src/main/java/com/duszki/blackjack/server/Card/Deck.java
@@ -0,0 +1,28 @@
+package com.duszki.blackjack.server.Card;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+import static com.duszki.blackjack.server.Card.Type.*;
+
+public class Deck {
+    public static final int STRONGCARD = 10;
+    private ArrayList<Card> deck;
+
+    public Deck(){
+        deck = new ArrayList<>();
+        addCards(Spades);
+        addCards(Hearts);
+        addCards(Diamonds);
+        addCards(Clubs);
+        Collections.shuffle(deck);
+    }
+
+    void addCards(Type colors){
+        for (int i = 1; i < 14; i++) {
+            Card card = new Card(i,colors);
+            deck.add(card);
+        }
+    }
+}
