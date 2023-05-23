1695bbfee1e83f63228254c87bc18cc17d87054c
LuckyCoin2 <240730@edu.p.lodz.pl>
2023-05-23 20:55:19 +0200

    Dodanie zdj i modyfikacja

--- core/src/main/java/com/duszki/blackjack/Homepage.java
+++ core/src/main/java/com/duszki/blackjack/Homepage.java
@@ -2,53 +2,88 @@ package com.duszki.blackjack;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import java.awt.*;
 
-public class Homepage extends ApplicationAdapter {
-    private Texture homepage;
+public class Homepage implements Screen {
+    private Stage stage;
     private SpriteBatch batch;
+    private Texture backgroundTexture;
     private OrthographicCamera camera;
-    private FitViewport viewport;
+
+    private Skin helpSkin;
+
+    public Homepage() {
+        batch = new SpriteBatch();
+        stage = new Stage(new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        Gdx.input.setInputProcessor(stage);
+        backgroundTexture = new Texture("homepage.jpg");
+        helpSkin = new Skin(Gdx.files.internal("skins/Help.png"));
+        TextButton startButton = new TextButton("Start", helpSkin);
+        startButton.setPosition(Gdx.graphics.getWidth() / 2 - startButton.getWidth() / 2, 300);
+        startButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                dispose();
+            }
+        });
+
+        stage.addActor(startButton);
+    }
+
 
     @Override
-    public void create() {
-        homepage = new Texture("homepage.jpg");
-        batch = new SpriteBatch();
-        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        viewport = new FitViewport(homepage.getWidth(), homepage.getHeight(), camera);
-
+    public void show() {
 
     }
 
     @Override
-    public void render() {
+    public void render(float delta) {
         ScreenUtils.clear(0, 0, 0, 0);
 
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        batch.draw(homepage, 0, 0);
+        batch.draw(backgroundTexture, 0, 0);
         batch.end();
-
     }
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height, true);
-        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
-        viewport.apply();
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
 
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        homepage.dispose();
-
+    stage.dispose();
     }
 }
--- lwjgl3/src/main/java/com/duszki/blackjack/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/duszki/blackjack/lwjgl3/Lwjgl3Launcher.java
@@ -12,7 +12,7 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Homepage(), getDefaultConfiguration());
+        return new Lwjgl3Application(new Blackjack(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
