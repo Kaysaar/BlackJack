39164cdb093f5a3ba11042b805adae2a83d40db4
dziwicz123 <michal.dziwisz@poczta.onet.pl>
2023-05-23 18:08:33 +0200

    Creation of class Cart

---
+++ server/src/main/java/com/duszki/blackjack/server/Card/Card.java
@@ -0,0 +1,11 @@
+package com.duszki.blackjack.server.Card;
+
+public class Card {
+    private final int value;
+    private final Type color;
+
+    public Card(int value, Type color){
+        this.value = value;
+        this.color = color;
+    }
+}
