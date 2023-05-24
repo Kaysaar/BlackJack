8c53fc669cf69a0d3fa46a42e584762c6a158d38
Wiktor Heblewski <240681@edu.p.lodz.pl>
2023-05-24 02:00:41 +0200

    set up basic server structure

--- server/src/main/java/com/duszki/blackjack/server/ServerLauncher.java
+++ server/src/main/java/com/duszki/blackjack/server/ServerLauncher.java
@@ -1,8 +1,119 @@
 package com.duszki.blackjack.server;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.kryonet.Server;
+import com.duszki.blackjack.server.Network;
+import com.duszki.blackjack.server.Network.RegisterName;
+import com.duszki.blackjack.server.Network.UpdateNames;
+import com.duszki.blackjack.server.Network.ChatMessage;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 /** Launches the server application. */
 public class ServerLauncher {
-    public static void main(String[] args) {
-        // TODO Implement server application.
+    Server server;
+    public ServerLauncher () throws IOException {
+        server = new Server() {
+            protected Connection newConnection () {
+                // By providing our own connection implementation, we can store per
+                // connection state without a connection ID to state look up.
+                return new ServerConnettion();
+            }
+        };
+
+        // For consistency, the classes to be sent over the network are
+        // registered by the same method for both the client and server.
+        Network.register(server);
+
+        server.addListener(new Listener() {
+            public void received (Connection c, Object object) {
+                // We know all connections for this server are actually ChatConnections.
+                ServerConnettion connection = (ServerConnettion)c;
+
+                if (object instanceof Network.RegisterName) {
+                    // Ignore the object if a client has already registered a name. This is
+                    // impossible with our client, but a hacker could send messages at any time.
+                    if (connection.name != null) return;
+                    // Ignore the object if the name is invalid.
+                    String name = ((Network.RegisterName)object).name;
+                    if (name == null) return;
+                    name = name.trim();
+                    if (name.length() == 0) return;
+                    // Store the name on the connection.
+                    connection.name = name;
+                    // Send a "connected" message to everyone except the new client.
+                    Network.ChatMessage chatMessage = new Network.ChatMessage();
+                    chatMessage.text = name + " connected.";
+                    server.sendToAllExceptTCP(connection.getID(), chatMessage);
+                    // Send everyone a new list of connection names.
+                    updateNames();
+                    return;
+                }
+
+                if (object instanceof ChatMessage) {
+                    // Ignore the object if a client tries to chat before registering a name.
+                    if (connection.name == null) return;
+                    ChatMessage chatMessage = (ChatMessage)object;
+                    // Ignore the object if the chat message is invalid.
+                    String message = chatMessage.text;
+                    if (message == null) return;
+                    message = message.trim();
+                    if (message.length() == 0) return;
+                    // Prepend the connection's name and send to everyone.
+                    chatMessage.text = connection.name + ": " + message;
+                    server.sendToAllTCP(chatMessage);
+                    return;
+                }
+            }
+
+            public void disconnected (Connection c) {
+                ServerConnettion connection = (ServerConnettion)c;
+                if (connection.name != null) {
+                    // Announce to everyone that someone (with a registered name) has left.
+                    ChatMessage chatMessage = new ChatMessage();
+                    chatMessage.text = connection.name + " disconnected.";
+                    server.sendToAllTCP(chatMessage);
+                    updateNames();
+                }
+            }
+        });
+        server.bind(Network.port);
+        server.start();
+
+        // Open a window to provide an easy way to stop the server.
+        JFrame frame = new JFrame("Chat Server");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosed (WindowEvent evt) {
+                server.stop();
+            }
+        });
+        frame.getContentPane().add(new JLabel("Close to stop the chat server."));
+        frame.setSize(320, 200);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    void updateNames () {
+        Connection[] connections = server.getConnections();
+        ArrayList names = new ArrayList(connections.length);
+        for (int i = connections.length - 1; i >= 0; i--) {
+            ServerConnettion connection = (ServerConnettion) connections[i];
+            names.add(connection.name);
+        }
+        // Send the names to everyone.
+        UpdateNames updateNames = new UpdateNames();
+        updateNames.names = (String[])names.toArray(new String[names.size()]);
+        server.sendToAllTCP(updateNames);
+    }
+
+    static class ServerConnettion extends Connection {
+        public String name;
+    }
+        public static void main(String[] args) throws IOException {
+        new ServerLauncher();
     }
 }
