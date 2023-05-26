aacc2128145916c41aa853d29103851c0a1a3bd4
Matx1002 <35429241+Matx1002@users.noreply.github.com>
2023-05-26 17:14:07 +0200

    Removed unneeded  file

--- core/src/main/java/com/duszki/blackjack/Homepage.java
+++ core/src/main/java/com/duszki/blackjack/Homepage.java
@@ -1,11 +1,7 @@
 package com.duszki.blackjack;
 
-import com.badlogic.gdx.Audio;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.Audio.*;
-import com.badlogic.gdx.Gdx.*;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -18,9 +14,6 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-
-
-import java.awt.*;
 
 public class Homepage implements Screen {
     private Stage stage;
