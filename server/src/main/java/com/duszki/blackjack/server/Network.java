5368bb15c04c99de8c840aca3821c268313f2b93
Wiktor Heblewski <240681@edu.p.lodz.pl>
2023-05-28 21:53:30 +0200

    Add additional classes to register

--- server/src/main/java/com/duszki/blackjack/server/Network.java
+++ server/src/main/java/com/duszki/blackjack/server/Network.java
@@ -2,18 +2,22 @@ package com.duszki.blackjack.server;
 
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryonet.EndPoint;
+import com.esotericsoftware.kryonet.Server;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 
 public class Network {
-    static public final int port = 54555;
+    static public final int port = 5000;
     static public void register (EndPoint endPoint) {
         Kryo kryo = endPoint.getKryo();
+        kryo.register(LocalDateTime.class);
         kryo.register(String[].class);
+        kryo.register(Boolean.class);
+        kryo.register(Network.reqPing.class);
         kryo.register(Date.class);
-        kryo.register(Ping.class);
-        kryo.register(reqPing.class);
+        kryo.register(Network.Ping.class);
+
     }
 
     static public class Ping {
