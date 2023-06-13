bb23fb74324a45ce3dc1c1d24f986bb4b805ac12
Matx1002 <35429241+Matx1002@users.noreply.github.com>
2023-06-13 19:09:08 +0200

    Revert "Add changes to window display"

--- core/src/main/java/com/duszki/blackjack/Homepage.java
+++ core/src/main/java/com/duszki/blackjack/Homepage.java
@@ -4,18 +4,22 @@ import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.audio.Music;
 
 public class Homepage implements Screen {
+
 
 
     private Game game;
@@ -29,10 +33,8 @@ public class Homepage implements Screen {
     private ImageButton buttonHelp;
     private ImageButton buttonQuit;
 
-    private float width;
-    private float height;
-
-    private float aspectRatio;
+    private float Windth;
+    private float Height;
 
 
     public InputMultiplexer getMultiplexer() {
@@ -42,52 +44,41 @@ public class Homepage implements Screen {
     private InputMultiplexer multiplexer;
 
     private Music music;
-
     public Music getMusic() {
         return music;
     }
-
     public Game getGame() {
         return game;
     }
 
 
     public Homepage(Game game) {
-        this.game = game;
-
-        aspectRatio = (float) Gdx.graphics.getWidth() / (float) Gdx.graphics.getHeight();
-
-        height = 1000;
-        width = height * aspectRatio;
+        this.game =game;
+        Windth = Gdx.graphics.getWidth();
+        Height = Gdx.graphics.getHeight();
 
         batch = new SpriteBatch();
-
+        stage = new Stage(new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        MyInputProcessor myInputProcessor = new MyInputProcessor();
+        multiplexer = new InputMultiplexer(myInputProcessor,stage);
+        Gdx.input.setInputProcessor(multiplexer);
         backgroundTexture = new Texture("homepage.jpg");
         camera = new OrthographicCamera();
-//        camera.setToOrtho(false, width, height);
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        viewport = new FitViewport(backgroundTexture.getWidth(), backgroundTexture.getHeight(), camera);
 
-        viewport = new FitViewport(width, height, camera);
-
-        camera.position.set(width / 2f, height / 2f, 0);
-
-        viewport.apply();
-
-        stage = new Stage(viewport);
-
-        MyInputProcessor myInputProcessor = new MyInputProcessor();
-        multiplexer = new InputMultiplexer(myInputProcessor, stage);
-
-        Gdx.input.setInputProcessor(multiplexer);
 
         Instruction instruction = new Instruction(this);
-        instruction.getTable().setPosition(((width - instruction.getTable().getWidth()) / 2f), 300);
-        instruction.getTable().setSize(1500, 800);
+        instruction.getTable().setPosition(((Gdx.graphics.getWidth() - instruction.getTable().getWidth()) / 2f),300);
+        instruction.getTable().setSize(1500,800);
 
         Login login = new Login(this, game);
-        login.getTable().setPosition(((width - login.getTable().getWidth()) / 2f), 300);
+        login.getTable().setPosition(((Gdx.graphics.getWidth() - login.getTable().getWidth()) / 2f),300);
 
         Setting setting2 = new Setting(this);
-        setting2.getTable().setPosition(((width - setting2.getTable().getWidth()) / 2f), 300);
+        setting2.getTable().setPosition(((Gdx.graphics.getWidth() - setting2.getTable().getWidth()) / 2f),300);
+
+
 
 
         // Music
@@ -98,12 +89,12 @@ public class Homepage implements Screen {
         // Button play
         Skin play = new Skin(Gdx.files.internal("skins/Play/PlayButton.json"));
         buttonPlay = new ImageButton(play);
-        float buttonX = ((width - buttonPlay.getWidth()) / 2f);
-        buttonPlay.setPosition(buttonX, 500);
+        float buttonX = ((Gdx.graphics.getWidth() - buttonPlay.getWidth()) / 2f);
+        buttonPlay.setPosition(buttonX,500);
 
-        buttonPlay.addListener(new ClickListener() {
+        buttonPlay.addListener(new ClickListener(){
             @Override
-            public void clicked(InputEvent event, float x, float y) {
+            public void clicked(InputEvent event, float x, float y){
                 stage.clear();
                 stage.addActor(login.getTable());
             }
@@ -112,10 +103,10 @@ public class Homepage implements Screen {
         //Button setting
         Skin setting = new Skin(Gdx.files.internal("skins/Settings/SettingsButton.json"));
         buttonSetting = new ImageButton(setting);
-        buttonSetting.setPosition(buttonX, 400);
-        buttonSetting.addListener(new ClickListener() {
+        buttonSetting.setPosition(buttonX,400);
+        buttonSetting.addListener(new ClickListener(){
             @Override
-            public void clicked(InputEvent event, float x, float y) {
+            public void clicked(InputEvent event, float x, float y){
                 stage.clear();
                 stage.addActor(setting2.getTable());
             }
@@ -125,22 +116,22 @@ public class Homepage implements Screen {
         //Buttton help
         Skin help = new Skin(Gdx.files.internal("skins/Help/HelpButton.json"));
         buttonHelp = new ImageButton(help);
-        buttonHelp.setPosition(buttonX, 300);
-        buttonHelp.addListener(new ClickListener() {
+        buttonHelp.setPosition(buttonX,300);
+        buttonHelp.addListener(new ClickListener(){
             @Override
-            public void clicked(InputEvent event, float x, float y) {
-                stage.clear();
-                stage.addActor(instruction.getTable());
+            public void clicked(InputEvent event, float x, float y){
+            stage.clear();
+            stage.addActor(instruction.getTable());
             }
         });
 
         //Button quit
         Skin quit = new Skin(Gdx.files.internal("skins/Quit/QuitButton.json"));
         buttonQuit = new ImageButton(quit);
-        buttonQuit.setPosition(buttonX, 200);
-        buttonQuit.addListener(new ClickListener() {
+        buttonQuit.setPosition(buttonX,200);
+        buttonQuit.addListener(new ClickListener(){
             @Override
-            public void clicked(InputEvent event, float x, float y) {
+            public void clicked(InputEvent event, float x, float y){
                 Gdx.app.exit();
             }
         });
@@ -149,6 +140,7 @@ public class Homepage implements Screen {
         stage.addActor(buttonQuit);
         stage.addActor(buttonSetting);
     }
+
 
 
     @Override
@@ -163,10 +155,9 @@ public class Homepage implements Screen {
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0, 0, 0, 0);
-        camera.update();
 
-        batch.begin();
         batch.setProjectionMatrix(camera.combined);
+        batch.begin();
         batch.draw(backgroundTexture, 0, 0);
         batch.end();
         stage.act();
@@ -175,25 +166,26 @@ public class Homepage implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height);
+        stage.getViewport().update(width, height, true);
 
 
-//        if (!Gdx.graphics.isFullscreen()) {
-//            float scale = Math.min(1600f / this.width, 1200f / this.height);
-//            Array<Actor> actors = stage.getActors();
-//            for (Actor actor : actors) {
-//                actor.setScale(scale);
-//            }
-//        } else {
-//            Array<Actor> actors = stage.getActors();
-//            for (Actor actor : actors) {
-//                actor.setScale(1);
-//            }
-//        }
-//        camera.setToOrtho(false, width, height);
-//        viewport.update(width, height, true);
-//        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
-
+        if(!Gdx.graphics.isFullscreen()) {
+            float scale = Math.min(1600f / Windth, 1200f / Height);
+            Array<Actor> actors = stage.getActors();
+            for (Actor actor : actors) {
+                actor.setScale(scale);
+            }
+        }else
+        {
+            Array<Actor> actors = stage.getActors();
+            for (Actor actor : actors) {
+                actor.setScale(1);
+            }
+        }
+        camera.setToOrtho(false, width, height);
+        viewport.update(width, height, true);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        viewport.apply();
     }
 
     @Override
