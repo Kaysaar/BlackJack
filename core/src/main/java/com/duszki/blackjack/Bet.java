14d4e0de62832e7db6ad8042084ccec02c5d5d86
LuckyCoin2 <240730@edu.p.lodz.pl>
2023-06-20 23:09:07 +0200

    Making changes to Bet

--- core/src/main/java/com/duszki/blackjack/Bet.java
+++ core/src/main/java/com/duszki/blackjack/Bet.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.Align;
@@ -17,6 +18,7 @@ public class Bet  {
     private float aspectRatio;
     private Table table;
     private Board board;
+    private TextField textField;
 
     public Bet(Board board) {
 
@@ -29,7 +31,7 @@ public class Bet  {
         table = new Table();
         table.setBackground(bet.getDrawable("back2_0"));
         table.setSize(300, 200);
-        TextField textField = new TextField("", bet);
+        textField = new TextField("", bet);
         table.add(textField).padTop(70.0f);
         table.setPosition(width - width / 5,500);
 
@@ -38,6 +40,7 @@ public class Bet  {
     public Table getTable() {
         return table;
     }
+
 
 
 }
