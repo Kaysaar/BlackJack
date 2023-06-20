2ddfcacc46b943854ccc844b10587918d4cfbc52
LuckyCoin2 <240730@edu.p.lodz.pl>
2023-06-20 23:37:45 +0200

    Making changes and adding clicklistener to button

--- core/src/main/java/com/duszki/blackjack/Bet.java
+++ core/src/main/java/com/duszki/blackjack/Bet.java
@@ -6,8 +6,10 @@ import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
@@ -19,6 +21,7 @@ public class Bet  {
     private Table table;
     private Board board;
     private TextField textField;
+    private String string;
 
     public Bet(Board board) {
 
@@ -27,23 +30,36 @@ public class Bet  {
         width = height * aspectRatio;
         this.board = board;
 
-        Skin bet = new Skin(Gdx.files.internal("new_bet/new_bet.json"));
-        table = new Table();
-        table.setBackground(bet.getDrawable("back2_0"));
-        table.setSize(300, 200);
 
-        TextField textField = new TextField("", bet);
-        table.add(textField).padTop(200.0f).row();
-        table.setPosition(width - width / 5,500);
-
+        final Skin[] bet = {new Skin(Gdx.files.internal("new_bet/new_bet.json"))};
         Skin bet_button = new Skin(Gdx.files.internal("bet_button/bet_button.json"));
         ImageButton imageButton = new ImageButton(bet_button, "default");
-        table.add(imageButton).padTop(40.0f).row();
-        table.setPosition(width - width / 5,500);
+        table = new Table();
+        table.padTop(150f);
+        table.setBackground(bet[0].getDrawable("back2_0"));
+        table.setSize(500, 300);
+
+        TextField textField = new TextField("", bet[0]);
+        table.add(textField).padRight(20f);
+        imageButton.setSize(50,50);
+        table.add(imageButton);
+        table.setPosition(width/2 - 5 * imageButton.getWidth(),500 - 2*imageButton.getHeight());
+        imageButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                string = textField.getText();
+                textField.setText("");
+            }
+        });
+
     }
 
     public Table getTable() {
         return table;
+    }
+
+    public String getBet(){
+        return this.string;
     }
 
 
